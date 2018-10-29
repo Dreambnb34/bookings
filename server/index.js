ba9fa3167818dd43639b14c150f6cc0b0dfe7435
Rohan Nobis <nobisro@gmail.com>
2018-10-29 15:49:14 -0400

    updated server, but not serving react pages yet

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/', (req, res) => {
   res.send(200);
