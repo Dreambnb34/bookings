ba7e9f4b630095ba76672733ef5075d6a6d95db1
Rohan Nobis <nobisro@gmail.com>
2018-10-31 15:30:49 -0400

    added second calendar and connected them via left and right buttons, which update month

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,18 +6,49 @@ import Calendar from './components/Calendar.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      leftYear: 2018,
+      leftMonth: 10,
+      rightYear: 2018,
+      rightMonth: 11,
+    };
+
+    this.incrementMonth = this.incrementMonth.bind(this);
+    this.decrementMonth = this.decrementMonth.bind(this);
+  }
+
+  incrementMonth() {
+    this.setState({
+      leftMonth: this.state.leftMonth + 1,
+      rightMonth: this.state.rightMonth + 1,
+    });
+  }
+
+  decrementMonth() {
+    this.setState({
+      leftMonth: this.state.leftMonth - 1,
+      rightMonth: this.state.rightMonth - 1,
+    });
   }
 
   render() {
     return (
-      <div>
-        <Calendar year={2018} month={10} />
+      <div className="xl-calendar-container">
+        <Calendar
+          leftMount={true}
+          year={this.state.leftYear}
+          month={this.state.leftMonth}
+          updateMonth={this.decrementMonth}
+        />
+        <Calendar
+          leftMount={false}
+          year={this.state.rightYear}
+          month={this.state.rightMonth}
+          updateMonth={this.incrementMonth}
+        />
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <App year={2018} month={9} />,
-  document.getElementById('availability'),
-);
+ReactDOM.render(<App />, document.getElementById('availability'));
