7ddb412c6b5210291e380c8d1cce953f3ab81e6c
Rohan Nobis <nobisro@gmail.com>
2018-10-31 18:20:48 -0400

    set up basic server with single route

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,12 @@
-const mysql = require("mysql");
-const knex = require("knex")({
-  client: "mysql",
+const mysql = require('mysql');
+const knex = require('knex')({
+  client: 'mysql',
   connection: {
-    host: "127.0.0.1",
-    user: "student",
-    password: "student",
-    database: "availability"
-  }
+    host: '127.0.0.1',
+    user: 'student',
+    password: 'student',
+    database: 'availability',
+  },
 });
+
+module.exports = knex;
