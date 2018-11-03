2e67bbdb2447cfc6dd950a739b433f3df5d9445e
Rohan Nobis <nobisro@gmail.com>
2018-11-03 19:40:36 -0400

    added initial snapshot testing to avoid complete app build

--- client/src/__tests__/Calendar.spec.js
+++ client/src/__tests__/Calendar.spec.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent, waitForElement } from 'react-testing-library';
+import axiosMock from 'axios';
 import Calendar from '../components/Calendar';
+import renderer from 'react-test-renderer';
 
 const mockData = {
   month: 10,
   year: 2018,
   leftMount: true,
+};
+
+const mockData2 = {
+  leftMount: true,
+  year: 2018,
+  month: 10,
+  bookings: [5],
+  minimum_stay: undefined,
+  getBookings: () => {
+    let id = Number(window.location.href.split('/')[4]);
+    axios.get(`/api/rooms/${id}`).then(data => {
+      this.sortBookings(data);
+    });
+  },
+  updateMonth: () => {},
 };
 
 it('is rendering out a wrapper div holding a table', () => {
@@ -15,20 +32,50 @@ it('is rendering out a wrapper div holding a table', () => {
   //debug(calendarContainer);
 });
 
-it('should have a table and th element with text content of "Su', () => {
+test('should have a table and th element with text content of "Su', () => {
   const { getByTestId } = render(<Calendar month={9} year={2018} />);
 
   expect(getByTestId('calendar-table-th-Su')).toHaveTextContent('Su');
 });
 
-it('should render out a date cell for Noverber 11', () => {
+test('should render out a date cell for Noverber 11', () => {
   const { getByTestId } = render(<Calendar month={10} year={2018} />);
-  expect(getByTestId('available-unselected-calendar-day')).toHaveTextContent(
+  expect(getByTestId('available-unselected-calendar-day-11')).toHaveTextContent(
     11,
   );
 });
 
 it('should render a div with the role button', () => {
   const { getByTestId } = render(<Calendar month={10} year={2018} />);
-  expect(getByTestId('calendar-button')).not.toBeNull();
+  expect(getByTestId('calendar-left-button')).not.toBeNull();
+});
+
+// it('should make GET request on click', () => {
+//   axiosMock.get.mockResolvedValueOnce({ data: [{}, {}, {}] });
+//   const url = '/api/rooms/44';
+//   const { getByText, getByTestId, container, asFragment } = render(
+//     <Calendar {...mockGetRequest} />,
+//   );
+
+//   fireEvent.click(getByText('left'))
+
+//   const leftButton = await waitForElement(() => getByTestId('calendar-left-button'))
+
+//   expect(axiosMock.get).toHaveBeenCalledTimes(1)
+//   expect(axiosMock.get).toHaveBeenCalledWith(url)
+//   expect(getByTestId('calendar-left-button'))
+// });
+
+it('should return true if true', () => {
+  expect(true).toBe(true);
+});
+
+it('should return null iff null', () => {
+  expect(null).toBe(null);
+});
+
+it('should render correctly', () => {
+  const tree = renderer.create(<Calendar {...mockData2} />).toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
