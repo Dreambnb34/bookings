c3d165132604967adc5bf8f90b75cccf70a72985
Rohan Nobis <nobisro@gmail.com>
2018-11-02 22:19:56 -0400

    added tests for table, tr to Calendar component

--- client/src/__tests__/Calendar.spec.js
+++ client/src/__tests__/Calendar.spec.js
@@ -2,6 +2,26 @@ import React from 'react';
 import { render } from 'react-testing-library';
 import Calendar from '../components/Calendar';
 
-it('is rendering out four rows', () => {
-  const { getByLabelText, debug } = render(<Calendar month={9} year={2018} />);
+const mockData = {
+  month: 10,
+  year: 2018,
+  leftMount: true,
+};
+
+it('is rendering out a wrapper div holding a table', () => {
+  const { getByTestId, debug } = render(<Calendar month={9} year={2018} />);
+  const calendarContainer = getByTestId('calendar-container');
+  expect(getByTestId('calendar-container')).toBeDefined();
+  //debug(calendarContainer);
+});
+
+it('should have a table and th element with text content of "Su', () => {
+  const { getByTestId } = render(<Calendar month={9} year={2018} />);
+
+  expect(getByTestId('calendar-table-th-Su')).toHaveTextContent('Su');
+});
+
+it('should render out a date cell for Noverber 11', () => {
+  const { getByTestId } = render(<Calendar month={10} year={2018} />);
+  expect(getByTestId('date-cell-11')).toHaveTextContent(11);
 });
